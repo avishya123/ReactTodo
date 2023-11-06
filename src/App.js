acbd083ea5b6a93503378e01209be4ff8e57a976
avishya <avishyaachu@gmail.com>
2023-11-06 17:37:07 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+// import logo from './logo.svg';
+// import './App.css';
+
+import Todolist from "./Components/AddTodo";
+import './Components/Todo.css';
+
+function App() {
+
+  return (
+<div>  
+  <Todolist/>
+    </div>
+  );
+}
+
+export default App;
